d4879616174e5749dd1ea3dc6190ae3fe8eab6f0
Jan Koeppen <jan@yaneq.com>
2023-12-02 01:09:38 -0500

    prettify all

--- pages/api/recommendations.ts
+++ pages/api/recommendations.ts
@@ -1,62 +1,61 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NearTextType } from "types";
-import type { NextApiRequest, NextApiResponse } from "next";
-import weaviate, { WeaviateClient, ApiKey } from "weaviate-ts-client";
+import { NearTextType } from 'types'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import weaviate, { WeaviateClient, ApiKey } from 'weaviate-ts-client'
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Object>
+  res: NextApiResponse<Object>,
 ) {
   try {
-    const { method } = req;
-    let { query } = req.body;
+    const { method } = req
+    let { query } = req.body
 
     const weaviateClusterUrl = process.env.WEAVIATE_CLUSTER_URL?.replace(
-      "https://",
-      ""
-    );
+      'https://',
+      '',
+    )
 
     switch (method) {
-      case "POST": {
+      case 'POST': {
         const client: WeaviateClient = weaviate.client({
-          scheme: "https",
+          scheme: 'https',
           host:
             weaviateClusterUrl ||
-            "zxzyqcyksbw7ozpm5yowa.c0.us-west2.gcp.weaviate.cloud",
+            'zxzyqcyksbw7ozpm5yowa.c0.us-west2.gcp.weaviate.cloud',
           apiKey: new ApiKey(
             process.env.WEAVIATE_API_KEY ||
-              "n6mdfI32xrXF3DH76i8Pwc2IajzLZop2igb6"
+              'n6mdfI32xrXF3DH76i8Pwc2IajzLZop2igb6',
           ), //READONLY API Key, ensure the environment variable is an Admin key to support writing
           headers: {
-            "X-OpenAI-Api-Key": process.env.OPENAI_API_KEY!,
+            'X-OpenAI-Api-Key': process.env.OPENAI_API_KEY!,
           },
-        });
+        })
 
         let nearText: NearTextType = {
           concepts: [],
-        };
+        }
 
-        nearText.certainty = 0.6;
-
-        nearText.concepts = query;
+        nearText.certainty = 0.6
+        nearText.concepts = query
 
         const recData = await client.graphql
           .get()
-          .withClassName("Movie")
-          .withFields("title summary poster")
+          .withClassName('Movie')
+          .withFields('title summary poster rating_average genre')
           .withNearText(nearText)
-          .withLimit(20)
-          .do();
+          .withLimit(40)
+          .do()
 
-        res.status(200).json(recData);
-        break;
+        res.status(200).json(recData)
+        break
       }
       default:
-        res.status(400);
-        break;
+        res.status(400)
+        break
     }
   } catch (err) {
-    console.error(err);
-    res.status(500);
+    console.error(err)
+    res.status(500)
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,78 +1,78 @@
-import { SyntheticEvent, useState } from "react";
-import CircleLoader from "react-spinners/CircleLoader";
-import Modal from "react-modal";
-import { IMovie } from "types";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { SyntheticEvent, useState } from 'react'
+import CircleLoader from 'react-spinners/CircleLoader'
+import Modal from 'react-modal'
+import { IMovie } from 'types'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
 
 const customStyles = {
   content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    width: "90%",
-    height: "80%",
-    transform: "translate(-50%, -50%)",
-    borderRadius: "5px",
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    width: '90%',
+    height: '80%',
+    transform: 'translate(-50%, -50%)',
+    borderRadius: '5px',
   },
-};
+}
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [loadedOnce, setLoadedOnce] = useState(false);
-  const [query, setQuery] = useState("");
-  const [recommendedMovies, setRecommendedMovies] = useState([]);
-  const [modalIsOpen, setIsOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
+  const [loadedOnce, setLoadedOnce] = useState(false)
+  const [query, setQuery] = useState('')
+  const [recommendedMovies, setRecommendedMovies] = useState([])
+  const [modalIsOpen, setIsOpen] = useState(false)
   const [selectedMovie, setSelectedMovie] = useState<IMovie | undefined>(
-    undefined
-  );
+    undefined,
+  )
 
   const openModal = (movie_title: string) => {
     const movieSelection = recommendedMovies.filter((movie: IMovie) => {
-      return movie.title === movie_title;
-    });
-    console.log(movieSelection);
-    setSelectedMovie(movieSelection[0]);
-    setIsOpen(true);
-  };
+      return movie.title === movie_title
+    })
+    console.log(movieSelection)
+    setSelectedMovie(movieSelection[0])
+    setIsOpen(true)
+  }
 
   const closeModal = () => {
-    setIsOpen(false);
-  };
+    setIsOpen(false)
+  }
 
   const getRecommendations = async (e: SyntheticEvent) => {
-    e.preventDefault();
+    e.preventDefault()
 
     // Check Inputs
-    if (query === "") {
-      alert("Please let us know what you'd like to learn!");
-      return;
+    if (query === '') {
+      alert("Please let us know what you'd like to learn!")
+      return
     }
 
-    setIsLoading(true);
+    setIsLoading(true)
 
-    await fetch("/api/recommendations", {
-      method: "POST",
+    await fetch('/api/recommendations', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         query,
       }),
     })
-      .then((res) => {
-        console.log(res);
-        if (res.ok) return res.json();
+      .then(res => {
+        console.log(res)
+        if (res.ok) return res.json()
       })
-      .then((recommendations) => {
-        console.log(recommendations.data.Get.Movie);
-        setRecommendedMovies(recommendations.data.Get.Movie);
-      });
+      .then(recommendations => {
+        console.log(recommendations.data.Get.Movie)
+        setRecommendedMovies(recommendations.data.Get.Movie)
+      })
 
-    setIsLoading(false);
-    setLoadedOnce(true);
-  };
+    setIsLoading(false)
+    setLoadedOnce(true)
+  }
 
   return (
     <div className="h-screen flex flex-col justify-between bg-gray-100">
@@ -80,16 +80,14 @@ export default function Home() {
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         style={customStyles}
-        contentLabel="Example Modal"
-      >
+        contentLabel="Example Modal">
         <div className="flex justify-between">
           <h3 className="mt-2 text-lg font-semibold text-gray-700">
             {selectedMovie?.title}
           </h3>
           <Button
             className="hover:font-bold rounded hover:bg-gray-700 p-2 w-20 hover:text-white "
-            onClick={closeModal}
-          >
+            onClick={closeModal}>
             Close
           </Button>
         </div>
@@ -98,43 +96,18 @@ export default function Home() {
             <div className="w-48 h-72">
               <img
                 src={selectedMovie?.poster}
-                alt={"Poster of the movie " + selectedMovie?.poster}
+                alt={'Poster of the movie ' + selectedMovie?.poster}
                 className="w-full h-full rounded-lg shadow-lg"
               />
             </div>
           </div>
           <div>
-            {/* <p className="mt-1 text-gray-500">
-              <span className="font-bold">Authors</span>:{" "}
-              {selectedMovie?.authors}
-            </p>
             <p>
-              <span className="font-bold">Genre</span>:{" "}
-              {selectedBook?.categories}
+              <span className="font-bold">Rating</span>:{' '}
+              {selectedMovie?.rating_average}
             </p>
-            <p>
-              <span className="font-bold">Rating</span>:{" "}
-              {selectedBook?.average_rating}
-            </p>
-            <p>
-              <span className="font-bold">Publication Year</span>:{" "}
-              {selectedBook?.published_year}
-            </p> */}
             <br />
             <p>{selectedMovie?.summary}</p>
-
-            {/* <div className="flex justify-center"> */}
-            {/* <a
-                className="hover:animate-pulse"
-                target="_blank"
-                href={"https://www.amazon.com/s?k=" + selectedBook?.isbn10}
-              >
-                <img
-                  className="w-60"
-                  src="https://kentuckynerd.com/wp-content/uploads/2019/05/amazon-buy-now-button.jpg"
-                />
-              </a> */}
-            {/* </div> */}
           </div>
         </div>
       </Modal>
@@ -147,13 +120,11 @@ export default function Home() {
           <form
             id="recommendation-form"
             className="mb-10"
-            onSubmit={getRecommendations}
-          >
+            onSubmit={getRecommendations}>
             <div className="mb-4">
               <label
                 htmlFor="favorite-books"
-                className="block text-gray-700 font-bold mb-2"
-              >
+                className="block text-gray-700 font-bold mb-2">
                 What are you afraid of?
               </label>
               <Input
@@ -163,8 +134,8 @@ export default function Home() {
                 placeholder="Jellyfish..."
                 className="block w-full px-4 py-2 border border-gray-300 bg-white rounded-md shadow-sm "
                 value={query}
-                onChange={(e) => {
-                  setQuery(e.target.value);
+                onChange={e => {
+                  setQuery(e.target.value)
                 }}
               />
             </div>
@@ -172,8 +143,7 @@ export default function Home() {
               className="bg-black text-white w-full rounded-md hover:bg-gray-800 hover:text-white"
               disabled={isLoading}
               type="submit"
-              variant="outline"
-            >
+              variant="outline">
               Get Scared
             </Button>
           </form>
@@ -181,7 +151,7 @@ export default function Home() {
           {isLoading ? (
             <div className="w-full flex justify-center h-60 pt-10">
               <CircleLoader
-                color={"#000000"}
+                color={'#000000'}
                 loading={isLoading}
                 size={100}
                 aria-label="Loading"
@@ -197,8 +167,7 @@ export default function Home() {
                   </h2>
                   <div
                     id="recommended-books"
-                    className="flex overflow-x-scroll pb-10 hide-scroll-bar"
-                  >
+                    className="flex overflow-x-scroll pb-10 hide-scroll-bar">
                     {/* <!-- Recommended books dynamically added here --> */}
                     <section className="container mx-auto mb-12">
                       <div className="flex flex-wrap -mx-2">
@@ -206,8 +175,7 @@ export default function Home() {
                           return (
                             <div
                               key={movie.title}
-                              className="w-full md:w-1/3 px-2 mb-4 animate-pop-in"
-                            >
+                              className="w-full md:w-1/3 px-2 mb-4 animate-pop-in">
                               <div className="bg-white p-6 flex items-center flex-col">
                                 <h3 className="text-xl font-semibold mb-4 line-clamp-1">
                                   {movie.title}
@@ -215,28 +183,27 @@ export default function Home() {
                                 <div className="w-48 h-72">
                                   <img
                                     src={movie.poster}
-                                    alt={"Poster of the movie " + movie.title}
+                                    alt={'Poster of the movie ' + movie.title}
                                     className="w-full h-full rounded-lg shadow-lg"
                                   />
                                 </div>
-                                {/* <p className="mt-4 text-gray-500 line-clamp-1">
-                                  {book.authors}
-                                </p> */}
-                                {/* <div className="flex">
+                                <p className="mt-4 text-gray-500 line-clamp-1">
+                                  Rating: {movie.rating_average}
+                                </p>
+                                <div className="flex mt-5">
                                   <Button
                                     className="bg-black text-white w-full rounded-md hover:bg-gray-800 hover:text-white"
                                     type="submit"
                                     variant="outline"
                                     onClick={() => {
-                                      openModal(book.title);
-                                    }}
-                                  >
+                                      openModal(movie.title)
+                                    }}>
                                     Learn More
                                   </Button>
-                                </div> */}
+                                </div>
                               </div>
                             </div>
-                          );
+                          )
                         })}
                       </div>
                     </section>
@@ -255,35 +222,31 @@ export default function Home() {
         <a
           href="https://x.com/yaneq/"
           target="_blank"
-          className="underline text-blue-200"
-        >
+          className="underline text-blue-200">
           @yaneq
         </a>
         &nbsp;- forked from&nbsp;
         <a
           href="https://github.com/weaviate/BookRecs"
           target="_blank"
-          className="underline text-blue-200"
-        >
+          className="underline text-blue-200">
           BookRecs
         </a>
         &nbsp;by&nbsp;
         <a
           href="https://x.com/aj__chan/"
           target="_blank"
-          className="underline text-blue-200"
-        >
+          className="underline text-blue-200">
           @aj__chan
         </a>
         &nbsp;-&nbsp;built with&nbsp;
         <a
           target="_blank"
           href="https://weaviate.io/"
-          className="underline text-blue-200"
-        >
+          className="underline text-blue-200">
           Weaviate
         </a>
       </footer>
     </div>
-  );
+  )
 }
